01ba5ae885f4a2dae0bc56195306ae78c500f823
Hanuman Yadav <hanuman2241@gmail.com>
2024-03-22 10:03:16 +0530

    'Blog'

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+
+  blogTitle:string='';
+  blogdetails:string='';
+  imageURL:string='';
+  blogURL:string='';
+
+}
